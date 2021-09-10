c66dcfd73fab2927225066e4f8bfb1ba9bcb3115
asiwaju07 <kazeem.asiwaju-bello@thepfs.biz>
2021-09-10 19:38:59 +0100

    Initial Commit

---
+++ Social-Identity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Social_Identity.MvcApplication" Language="C#" %>
---
+++ Social-Identity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Social_Identity.Startup))]
+namespace Social_Identity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
